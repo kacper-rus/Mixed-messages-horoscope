3633a0f8b95134912c7eacb27a03560497cf8cf1
Kacper <racper202@gmail.com>
2021-08-31 10:01:59 +0200

    Today's date

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("hey")
+
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth() + 1).padStart(2, '0');
+var yyyy = today.getFullYear();
+
+today = dd + '/' + mm + '/' + yyyy;
+console.log("Welcome to your today's horoscope for "+today)
+
